cccba437436c327f0d0fbda6ba8e75f4a91e7816
ismail-dev-code <hm.ismail772@gmail.com>
2025-11-13 20:54:58 +0600

    feat: more added

--- dist/test.js
+++ dist/test.js
@@ -3,3 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const course = "pro level development";
 console.log(course);
 //# sourceMappingURL=test.js.map
+
+const user: {
+  firstName: string,
+  middleName: string,
+  lastName: string,
+} = {
+  firstName: "Mohammad",
+  middleName: "Ismail",
+  lastName: "Hossain",
+};
+console.log(user);
--- src/primitive.ts
+++ src/primitive.ts
@@ -2,3 +2,24 @@
 // string , number, boolean, undefined, null
 // ts: never , unknown , void 
 let userName: string = "faisal123"
+
+userName = "isamil"
+
+let userId = 1342513
+userId = 252523
+userId.toFixed()
+
+
+let isAdmin = true
+isAdmin = false
+
+const user: {
+  firstName: string,
+  middleName: string,
+  lastName: string
+} = {
+  firstName: "Mohammad",
+  middleName: "Ismail",
+  lastName: "Hossain"
+}
+console.log(user);
